0ff3411ea06acf1377f55e2d77e133666776e7e7
ayutac <fly.high.android@gmail.com>
2025-04-04 02:49:45 +0200

    Reluctantly added the phoenix.

--- src/main/java/studio/abos.mc.mysticalcreatures/MysticalCreatures.java
+++ src/main/java/studio/abos.mc.mysticalcreatures/MysticalCreatures.java
@@ -1,11 +1,17 @@
 package studio.abos.mc.mysticalcreatures;
 
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.biome.Biome;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -30,25 +36,28 @@ import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import studio.abos.mc.mysticalcreatures.client.model.entity.PhoenixModel;
+import studio.abos.mc.mysticalcreatures.client.render.entity.PhoenixRenderer;
 import studio.abos.mc.mysticalcreatures.datagen.MyBiomeTagProvider;
 import studio.abos.mc.mysticalcreatures.datagen.MyBlockTagProvider;
 import studio.abos.mc.mysticalcreatures.datagen.MyItemTagProvider;
 import studio.abos.mc.mysticalcreatures.datagen.MyLanguageProvider;
 import studio.abos.mc.mysticalcreatures.datagen.MyModelProvider;
+import studio.abos.mc.mysticalcreatures.entity.PhoenixEntity;
+
+import java.util.function.Supplier;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(MysticalCreatures.MODID)
 public class MysticalCreatures
 {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "mysticalcreatures";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "mysticalcreatures" namespace
+
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "mysticalcreatures" namespace
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "mysticalcreatures" namespace
+    public static final DeferredRegister.Entities ENTITY_TYPES = DeferredRegister.createEntities(MODID);
+
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
     public static final TagKey<Item> PHOENIX_BREEDING_ITEMS = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(MODID, "phoenix_breeding_items"));
@@ -71,6 +80,22 @@ public class MysticalCreatures
     public static final DeferredItem<Item> UNICORN_HORN = ITEMS.registerSimpleItem("unicorn_horn", new Item.Properties().rarity(Rarity.RARE));
     public static final DeferredItem<Item> TROLL_HEART = ITEMS.registerSimpleItem("troll_heart", new Item.Properties().rarity(Rarity.RARE));
 
+    public static final Supplier<EntityType<PhoenixEntity>> PHOENIX_ENTITY = ENTITY_TYPES.register("phoenix",
+            () -> EntityType.Builder.of(PhoenixEntity::new, MobCategory.CREATURE)
+            .sized(1f, 1f)
+            .fireImmune()
+            .clientTrackingRange(8)
+            .build(ResourceKey.create(Registries.ENTITY_TYPE, ResourceLocation.fromNamespaceAndPath(MODID, "phoenix"))));
+
+    public static final ModelLayerLocation PHOENIX_LAYER = new ModelLayerLocation(
+            // Should be the name of the entity this layer belongs to.
+            // May be more generic if this layer can be used on multiple entities.
+            ResourceLocation.fromNamespaceAndPath(MODID, "phoenix_entity"),
+            // The name of the layer itself. Should be main for the entity's base model,
+            // and a more descriptive name (e.g. "wings") for more specific layers.
+            "head"
+    );
+
     // Creates a creative tab with the id "mysticalcreatures:example_tab" for the example item, that is placed after the combat tab
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("mysticalcreatures", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.mysticalcreatures")) //The language key for the title of your CreativeModeTab
@@ -85,8 +110,7 @@ public class MysticalCreatures
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public MysticalCreatures(IEventBus modEventBus, ModContainer modContainer)
-    {
+    public MysticalCreatures(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -94,6 +118,8 @@ public class MysticalCreatures
         BLOCKS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so items get registered
         ITEMS.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so entity types get registered
+        ENTITY_TYPES.register(modEventBus);
         // Register the Deferred Register to the mod event bus so tabs get registered
         CREATIVE_MODE_TABS.register(modEventBus);
 
@@ -104,13 +130,13 @@ public class MysticalCreatures
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
+        modEventBus.addListener(this::onAttributeCreation);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
 //        LOGGER.info("HELLO FROM COMMON SETUP");
 //
@@ -123,8 +149,7 @@ public class MysticalCreatures
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.insertAfter(Items.PHANTOM_MEMBRANE.getDefaultInstance(), PHOENIX_FEATHER.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
             event.insertAfter(PHOENIX_FEATHER.get().getDefaultInstance(), JACKALOPE_ANTLERS.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
@@ -133,27 +158,38 @@ public class MysticalCreatures
         }
     }
 
+    public void onAttributeCreation(EntityAttributeCreationEvent event) {
+        event.put(PHOENIX_ENTITY.get(), PhoenixEntity.createPhoenixAttributes().build());
+    }
+
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
+    public void onServerStarting(ServerStartingEvent event) {
 //        // Do something when the server starts
 //        LOGGER.info("HELLO from server starting");
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
+        public static void onClientSetup(FMLClientSetupEvent event) {
 //            // Some client setup code
 //            LOGGER.info("HELLO FROM CLIENT SETUP");
 //            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
         }
+        @SubscribeEvent
+        public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+            event.registerLayerDefinition(PHOENIX_LAYER, PhoenixModel::createBodyLayer);
+        }
+
+        @SubscribeEvent
+        public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+            event.registerEntityRenderer(PHOENIX_ENTITY.get(), PhoenixRenderer::new);
+        }
     }
 
+    // datagen
     @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD)
     public static class DataHandler {
         @SubscribeEvent
---
+++ src/main/java/studio/abos/mc/mysticalcreatures/client/render/entity/PhoenixRenderer.java
@@ -0,0 +1,30 @@
+package studio.abos.mc.mysticalcreatures.client.render.entity;
+
+import net.minecraft.client.renderer.entity.AgeableMobRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import studio.abos.mc.mysticalcreatures.MysticalCreatures;
+import studio.abos.mc.mysticalcreatures.client.model.entity.PhoenixModel;
+import studio.abos.mc.mysticalcreatures.client.render.state.PhoenixRenderState;
+import studio.abos.mc.mysticalcreatures.entity.PhoenixEntity;
+
+@OnlyIn(Dist.CLIENT)
+public class PhoenixRenderer extends AgeableMobRenderer<PhoenixEntity, PhoenixRenderState, PhoenixModel> {
+
+    public PhoenixRenderer(EntityRendererProvider.Context context) {
+        super(context, new PhoenixModel(context.bakeLayer(MysticalCreatures.PHOENIX_LAYER)), new PhoenixModel(context.bakeLayer(MysticalCreatures.PHOENIX_LAYER)), 0.5f);
+
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(PhoenixRenderState phoenixRenderState) {
+        return ResourceLocation.fromNamespaceAndPath(MysticalCreatures.MODID, "phoenix_entity");
+    }
+
+    @Override
+    public PhoenixRenderState createRenderState() {
+        return new PhoenixRenderState();
+    }
+}
