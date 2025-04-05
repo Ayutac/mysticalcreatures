176224aaab52270542ab09ae07adfe3367ef2887
ayutac <fly.high.android@gmail.com>
2025-04-05 11:18:24 +0200

    Made Jackalope an attacker.

--- src/main/java/studio/abos/mc/mysticalcreatures/entity/JackalopeEntity.java
+++ src/main/java/studio/abos/mc/mysticalcreatures/entity/JackalopeEntity.java
@@ -1,10 +1,7 @@
 package studio.abos.mc.mysticalcreatures.entity;
 
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.TimeUtil;
-import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntitySelector;
-import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -13,13 +10,12 @@ import net.minecraft.world.entity.ai.goal.BreedGoal;
 import net.minecraft.world.entity.ai.goal.ClimbOnTopOfPowderSnowGoal;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import studio.abos.mc.mysticalcreatures.MysticalCreatures;
 
 public class JackalopeEntity extends AbstractMysticalCreature {
@@ -28,18 +24,24 @@ public class JackalopeEntity extends AbstractMysticalCreature {
     }
 
     public static AttributeSupplier.Builder createJackalopeAttributes() {
-        return Animal.createAnimalAttributes().add(Attributes.MAX_HEALTH, 15d).add(Attributes.MOVEMENT_SPEED, 0.25d);
+        return Animal.createAnimalAttributes()
+                .add(Attributes.MAX_HEALTH, 15d)
+                .add(Attributes.MOVEMENT_SPEED, 0.25d)
+                .add(Attributes.FOLLOW_RANGE, 20d)
+                .add(Attributes.ATTACK_DAMAGE, 3d);
     }
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(1, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new ClimbOnTopOfPowderSnowGoal(this, this.level()));
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(0, new ClimbOnTopOfPowderSnowGoal(this, this.level()));
+        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.5f, true));
         this.goalSelector.addGoal(2, new BreedGoal(this, 0.8));
         this.goalSelector.addGoal(4, new AvoidEntityGoal<>(this, Animal.class, 10.0F, 2.2, 2.2,
                 livingEntity -> EntitySelector.NO_CREATIVE_OR_SPECTATOR.test(livingEntity) && livingEntity.getType().is(MysticalCreatures.JACKALOPE_AVOIDS)));
         this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 0.6));
         this.goalSelector.addGoal(11, new LookAtPlayerGoal(this, Player.class, 10.0F));
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
     }
 
     @Override
