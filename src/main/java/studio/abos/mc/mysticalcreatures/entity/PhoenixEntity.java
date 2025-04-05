6c61fecfafea930211bdbb83e3c8eb7528b59411
ayutac <fly.high.android@gmail.com>
2025-04-05 11:09:38 +0200

    Organized imports.

--- src/main/java/studio/abos/mc/mysticalcreatures/entity/PhoenixEntity.java
+++ src/main/java/studio/abos/mc/mysticalcreatures/entity/PhoenixEntity.java
@@ -13,7 +13,6 @@ import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseFireBlock;
-import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import studio.abos.mc.mysticalcreatures.MysticalCreatures;
--- src/main/java/studio/abos/mc/mysticalcreatures/entity/TrollEntity.java
+++ src/main/java/studio/abos/mc/mysticalcreatures/entity/TrollEntity.java
@@ -1,17 +1,11 @@
 package studio.abos.mc.mysticalcreatures.entity;
 
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.TimeUtil;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import studio.abos.mc.mysticalcreatures.MysticalCreatures;
 
 public class TrollEntity extends AbstractMysticalCreature {
